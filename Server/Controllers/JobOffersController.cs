374eb91ba1da9d12cfc472d793ccae41eb122075
9thCircle <the9thcircleofhelll@gmail.com>
2023-09-30 11:41:23 +0300

    Cleaned up main page

--- Server/Controllers/JobOffersController.cs
+++ Server/Controllers/JobOffersController.cs
@@ -1,33 +1,31 @@
 using FreeJob.Shared;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FreeJob.Server.Controllers
+namespace FreeJob.Server.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class JobOffersController : ControllerBase
 {
-    [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class JobOffersController : ControllerBase
+    private static readonly List<Job> Jobs = new();
+
+    private readonly ILogger<JobOffersController> _logger;
+
+    public JobOffersController(ILogger<JobOffersController> logger)
     {
-        private static readonly List<Job> Jobs = new();
+        _logger = logger;
+    }
 
-        private readonly ILogger<JobOffersController> _logger;
+    [HttpGet]
+    public IEnumerable<Job> Get()
+    {
+        return new Job[] { new Job("hello", "smth", 100, 1, false) };
+    }
 
-        public JobOffersController(ILogger<JobOffersController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<Job> Get()
-        {
-            return new Job[] { new Job("hello", "smth", 100, 1, false) };
-        }
-
-        [HttpPost]
-        public bool Post()
-        {
-            return true;
-        }
+    [HttpPost]
+    public bool Post()
+    {
+        return true;
     }
 }
